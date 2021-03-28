8ef1dc4646a79d0227e5f08240d5ba3a5a028289
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-03-28 15:47:18 -0300

    tests

--- src/test/java/com/ejemplojobs/math/MathUtilTest.java
+++ src/test/java/com/ejemplojobs/math/MathUtilTest.java
@@ -14,4 +14,22 @@ class MathUtilTest {
     void subtract() {
         assertEquals(2, MathUtil.subtract(4, 2));
     }
+
+
+    @Test
+    void multiply() {
+        assertEquals(4, MathUtil.multiply(2, 2));
+    }
+
+
+    @Test
+    void divide() {
+        assertEquals(2, MathUtil.divide(4, 2));
+    }
+
+
+    @Test
+    void rest() {
+        assertEquals(0, MathUtil.rest(4, 2));
+    }
 }
